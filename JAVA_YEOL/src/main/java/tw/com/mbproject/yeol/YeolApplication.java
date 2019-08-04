5c1bd890d2632065807a98950c1b89a71915aec0
Bruce Hsu <bruce770405@gmail.com>
2019-08-05 00:51:16 +0800

    init commit

---
+++ JAVA_YEOL/src/main/java/tw/com/mbproject/yeol/YeolApplication.java
@@ -0,0 +1,25 @@
+package tw.com.mbproject.yeol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 程式進入點.
+ * @author BruceHsu
+ * @version
+ * @serial
+ * @see
+ */
+@SpringBootApplication
+public class YeolApplication {
+
+	/**
+	 * main.
+	 * 
+	 * @param args the String[] args.
+	 */
+	public static void main(String[] args) {
+		SpringApplication.run(YeolApplication.class, args);
+	}
+
+}
