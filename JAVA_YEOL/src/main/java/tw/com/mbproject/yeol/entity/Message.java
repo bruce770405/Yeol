faf179fb82b6693e1108c05922fa253a401763fb
matt <bronx0807@gmail.com>
2019-09-21 18:02:02 +0800

    add more message entity fields

--- JAVA_YEOL/src/main/java/tw/com/mbproject/yeol/entity/Message.java
+++ JAVA_YEOL/src/main/java/tw/com/mbproject/yeol/entity/Message.java
@@ -5,26 +5,34 @@ import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 @Document(collection = "messages")
 public class Message {
 
     @Id
     public String id;
-    
+    @Field("authorId")
+    public String authorId;
+    @Field("authorName")
+    public String authorName;
     @Field("title")
     public String title;
     @Field("content")
     public String content;
     @Field("views")
     public Integer views;
-
-    public Message() {
-    }
-
-    public Message(String title) {
-        this.title = title;
-    }
+    @Field("up")
+    public Integer up;
+    @Field("down")
+    public Integer down;
+    @Field("createMs")
+    public Long createMs;
+    @Field("updateMs")
+    public Long updateMs;
+    @Field("deleteFlag")
+    public Boolean deleteFlag;
     
 }
