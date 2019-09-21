c9aa9841960b3e06ab5fd2a07274f9ee1c9f21cf
matt <bronx0807@gmail.com>
2019-09-21 16:20:41 +0800

    use lombok library to generate boilerplate code

--- JAVA_YEOL/src/main/java/tw/com/mbproject/yeol/entity/Message.java
+++ JAVA_YEOL/src/main/java/tw/com/mbproject/yeol/entity/Message.java
@@ -4,6 +4,9 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
 
+import lombok.Data;
+
+@Data
 @Document(collection = "messages")
 public class Message {
 
@@ -23,38 +26,5 @@ public class Message {
     public Message(String title) {
         this.title = title;
     }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public Integer getViews() {
-        return views;
-    }
-
-    public void setViews(Integer views) {
-        this.views = views;
-    }
-
     
 }
