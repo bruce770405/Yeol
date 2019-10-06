4686b24c40c647da599ab9b247bd3e7e111c9eaa
Bruce Hsu <bruce770405@gmail.com>
2019-10-06 23:24:30 +0800

    暫時性驗證只記錄前後往返時間

--- JAVA_YEOL/src/main/java/tw/com/mbproject/yeol/interceptor/AuthenInterceptor.java
+++ JAVA_YEOL/src/main/java/tw/com/mbproject/yeol/interceptor/AuthenInterceptor.java
@@ -1,19 +1,45 @@
 package tw.com.mbproject.yeol.interceptor;
 
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.server.ServerWebExchange;
 import org.springframework.web.server.WebFilter;
 import org.springframework.web.server.WebFilterChain;
 import reactor.core.publisher.Mono;
 
+/**
+ *
+ */
 public class AuthenInterceptor implements WebFilter {
 
+    /**
+     * log物件.
+     */
+    private static final Logger LOGGER = LoggerFactory.getLogger(AuthenInterceptor.class);
 
+
+    /**
+     * TODO 目前只紀錄service前後完成的時間.
+     *
+     * @param serverWebExchange
+     * @param webFilterChain
+     * @return the Mono Object
+     */
     @Override
     public Mono<Void> filter(ServerWebExchange serverWebExchange, WebFilterChain webFilterChain) {
-//        serverWebExchange.getResponse()
-//                .getHeaders().add("web-filter", "web-filter-test");
-//        return webFilterChain.filter(serverWebExchange);
-        return null;
+        long startTime = System.currentTimeMillis();
+        String path = serverWebExchange.getRequest().getURI().getPath();
+        LOGGER.info("Serving '{}'", path);
+
+        return webFilterChain.filter(serverWebExchange).doAfterTerminate(() -> {
+                    serverWebExchange.getResponse().getHeaders().entrySet().forEach(e ->
+                            LOGGER.info("Response header '{}': {}", e.getKey(), e.getValue()));
+
+                    LOGGER.info("Served '{}' as {} in {} msec",
+                            path,
+                            serverWebExchange.getResponse().getStatusCode(),
+                            System.currentTimeMillis() - startTime);
+                }
+        );
     }
 }
