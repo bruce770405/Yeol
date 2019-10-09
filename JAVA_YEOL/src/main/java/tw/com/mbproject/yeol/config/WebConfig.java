cdc73101169ca58ea4197bc4c00b3d6039bad25b
Bruce Hsu <bruce770405@gmail.com>
2019-10-09 18:40:46 +0800

    增加convert供通規則

--- JAVA_YEOL/src/main/java/tw/com/mbproject/yeol/config/WebConfig.java
+++ JAVA_YEOL/src/main/java/tw/com/mbproject/yeol/config/WebConfig.java
@@ -1,14 +1,21 @@
 package tw.com.mbproject.yeol.config;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.reactive.config.CorsRegistry;
+import org.springframework.web.reactive.config.EnableWebFlux;
 import org.springframework.web.reactive.config.ResourceHandlerRegistry;
 import org.springframework.web.reactive.config.WebFluxConfigurer;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import tw.com.mbproject.yeol.interceptor.AuthenInterceptor;
 
+@EnableWebFlux
 @Configuration
 @ComponentScan(basePackageClasses = {AuthenInterceptor.class})
 public class WebConfig implements WebFluxConfigurer {
@@ -37,5 +44,22 @@ public class WebConfig implements WebFluxConfigurer {
         registry.addMapping("/**").allowCredentials(true).allowedOrigins("*").allowedMethods("POST", "GET");
     }
 
+
+    /**
+     * 設定JSON資料在進入時的物件轉換Mapping規則.
+     *
+     * @return convert bean
+     */
+    @Bean(name = "MappingJackson2HttpMessageConverter")
+    public MappingJackson2HttpMessageConverter converter() {
+        ObjectMapper objectMapper = Jackson2ObjectMapperBuilder.json().serializationInclusion(JsonInclude.Include.NON_NULL)
+                .visibility(PropertyAccessor.FIELD, JsonAutoDetect.Visibility.ANY)
+                .visibility(PropertyAccessor.GETTER, JsonAutoDetect.Visibility.NONE)
+                .visibility(PropertyAccessor.SETTER, JsonAutoDetect.Visibility.NONE).build();
+
+        final MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+        converter.setObjectMapper(objectMapper);
+        return converter;
+    }
 
 }
