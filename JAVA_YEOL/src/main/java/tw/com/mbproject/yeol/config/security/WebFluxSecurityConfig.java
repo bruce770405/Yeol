7dd200033231eae0c00ec7ba2115729589d9068c
matt <bronx0807@gmail.com>
2019-10-12 20:05:59 +0800

    security switch setting

--- JAVA_YEOL/src/main/java/tw/com/mbproject/yeol/config/security/WebFluxSecurityConfig.java
+++ JAVA_YEOL/src/main/java/tw/com/mbproject/yeol/config/security/WebFluxSecurityConfig.java
@@ -1,5 +1,6 @@
 package tw.com.mbproject.yeol.config.security;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
@@ -10,15 +11,24 @@ import org.springframework.security.web.server.SecurityWebFilterChain;
 @EnableWebFluxSecurity
 public class WebFluxSecurityConfig {
     
+    @Value("${yeol.security.enable}")
+    private boolean isSecurityEnabled;
+    
     @Bean
     public SecurityWebFilterChain securitygWebFilterChain(ServerHttpSecurity http) {
-        return http.csrf().disable().formLogin().and()
-                .authorizeExchange()
-                .pathMatchers("/api/**").authenticated()
-                .pathMatchers("/join")
-                .permitAll()
-                .and()
-                .build();
+        
+        var spec = http.csrf().disable().formLogin().and()
+        .authorizeExchange();
+        if (isSecurityEnabled) {
+            spec = spec.pathMatchers("/api/**").authenticated().pathMatchers("/join")
+                    .permitAll();
+        } else {
+            spec = spec.pathMatchers("/**")
+            .permitAll();
+        }
+        
+        return spec.and().build();
+        
     }
     
     @Bean
